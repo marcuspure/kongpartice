8b3676c256969c3edf8ccd2b42dbcbec6beed4b1
marcuspure <oplasdfrank8@gmail.com>
2021-04-30 18:54:27 +0800

    footer

--- include/_footer.php
+++ include/_footer.php
@@ -1,9 +1,70 @@
-<div class="footerwrap">
+<div class="footerWrap">
    <footer id="footer" class="footer">
-      <div class="inner">
-
+      <div class="inner d-flex">
+         <a href="" class="footer_item">
+            <img class="footer_item_icon" src="https://fakeimg.pl/24x24/">
+            <span class="footer_item_text">首頁</span>
+         </a>
+         <a href="" class="footer_item">
+            <img class="footer_item_icon" src="https://fakeimg.pl/24x24/">
+            <span class="footer_item_text">追蹤事項</span>
+         </a>
+         <a href="" class="footer_item">
+            <img class="footer_item_icon" src="https://fakeimg.pl/24x24/">
+            <span class="footer_item_text">行事曆</span>
+         </a>
+         <a href="" class="footer_item">
+            <img class="footer_item_icon" src="https://fakeimg.pl/24x24/">
+            <span class="footer_item_text">客戶管理</span>
+         </a>
+         <a href="" class="footer_item">
+            <img class="footer_item_icon" src="https://fakeimg.pl/24x24/">
+            <span class="footer_item_text">更多</span>
+         </a>
       </div>
-      <div class="copyright">©<?php echo $siteName; ?> All Rights Reserved.</div>
-	</footer><!--End of footer-->
+      <!-- <div class="inner row">
+         <div class="footer_logo col">
+            <div class="logo">
+               <a href="./" class=" flex-1"><img src="https://fakeimg.pl/24x24/"  alt="<?php echo $siteName; ?>"></a>
+             </div>
+             <div class="footertext">
+                <label for="">首頁</label>
+             </div>
+         </div>
+         <div class="footer_logo col">
+            <div class="logo">
+               <a href="./" class="footer_logo flex-1"><img src="https://fakeimg.pl/24x24/"  alt="<?php echo $siteName; ?>"></a>
+             </div>
+             <div class="footertext">
+                <label for="">首頁</label>
+             </div>
+         </div>
+         <div class="footer_logo col">
+            <div class="logo">
+               <a href="./" class="footer_logo flex-1"><img src="https://fakeimg.pl/24x24/"  alt="<?php echo $siteName; ?>"></a>
+             </div>
+             <div class="footertext">
+                <label for="">首頁</label>
+             </div>
+         </div>
+         <div class="footer_logo col">
+            <div class="logo">
+               <a href="./" class="footer_logo flex-1"><img src="https://fakeimg.pl/24x24/"  alt="<?php echo $siteName; ?>"></a>
+             </div>
+             <div class="footertext">
+                <label for="">首頁</label>
+             </div>
+         </div>
+         <div class="footer_logo col">
+            <div class="logo">
+               <a href="./" class="footer_logo flex-1"><img src="https://fakeimg.pl/24x24/"  alt="<?php echo $siteName; ?>"></a>
+             </div>
+             <div class="footertext">
+                <label for="">首頁</label>
+             </div>
+         </div>
+      </div> -->
+   </footer><!--End of footer-->
 </div>
+
 <!-- 置底 -->
