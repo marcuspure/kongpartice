cd2b05293e7bf622dbb69e582a8e4b729548eb16
marcuspure <oplasdfrank8@gmail.com>
2021-04-30 17:12:19 +0800

    header切到一半

---
+++ include/_header.php
@@ -0,0 +1,13 @@
+<!-- header -->
+    <div class="headerWrap">
+        <header id="header" class="header font-serif">
+            <div class="inner">
+                <a href="" class="header_icon fas fa-bars"></a>
+                <a href="./" class="header_logo logo"><img src="https://fakeimg.pl/200x24/"  alt="<?php echo $siteName; ?>"></a>
+                <a id="navToggle" class="header_icon far fa-bell"></a>
+            </div>
+            <div class="headerbuttom"></div>
+        </header><!--End of header-->
+        
+    </div>
+
--- index.php
+++ index.php
@@ -4,23 +4,47 @@ $pageTitle = '';
 <!DOCTYPE html>
 <html lang="zh-Hant">
 <head>
-<?php include("include/meta.php"); ?>
+<?php include("include/_meta.php"); ?>
 </head>
 
 <body>
 <div id="wrap" class="wrap">
-	<?php include("include/header.php"); ?>
+	<?php include("include/_header.php"); ?>
 
 	<div id="main" class="main">
 		content 從這裡開始
+		<div class="item">
+  <div class="item_header">
+    <div class="addressList_tittle d-flex align-items-center">
+    <p class="mr-auto title">續保件</p>
+    <a class="btn btn-pill btn-outline btn-xsmall" 
+              href="" 
+              data-modal="#">編輯
+            </a> 
+    </div>
+  </div>
+  <div class="date">
+    <div class="title">
+      <label for="" class="date_title">全部</label>
+      <label for="" class="date_title">本月到期</label>
+      <label for="" class="date_title">7日到期</label>
+    </div>
+    <div class="expired">
+      <label for="" class="date_number">100</label>
+      <label for="" class="date_number">10</label>
+      <label for="" class="date_number">5</label>
+    </div>
+  </div>
+</div>
+	<?php include("include/_body.php"); ?>
 	</div><!--End of main-->
 
-	<?php include("include/footer.php"); ?>
+	<?php include("include/_footer.php"); ?>
 
 
 </div><!--End of wrap-->
 
-<?php include("include/js.php"); ?>
+<?php include("include/_js.php"); ?>
 
 </body>
 </html>
